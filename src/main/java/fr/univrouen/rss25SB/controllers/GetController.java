ff97c7259090f1c98c3e0b29000ff7a984080132
celinaFeghoul <celinafeghoul@gmail.com>
2025-05-13 20:02:44 +0200

    Configure JPA persistence with PostgreSQL, update Item entity and controller, and add ItemRepository

--- src/main/java/fr/univrouen/rss25SB/controllers/GetController.java
+++ src/main/java/fr/univrouen/rss25SB/controllers/GetController.java
@@ -1,29 +1,43 @@
 package fr.univrouen.rss25SB.controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class GetController {
+import fr.univrouen.rss25SB.Repository.ItemRepository;
+import fr.univrouen.rss25SB.model.Item;
 
-	@GetMapping("/resume") //Mappe une méthode à une requête HTTP GET
-	public String getListRSSinXML() {
-		return "Envoi de la liste des flux RSS";
-	}
+@RestController
+@RequestMapping("/items")
+public class GetController {
 	
-	@GetMapping("/guid")
-	public String getRSSinXML(
-			@RequestParam(value = "guid") String texte //Récupère un paramètre de l'URL
-			) {
-		return "Détail du contenu RSS demandé " + texte;
-	}
+	private final ItemRepository itemRepository;
 	
-	@GetMapping("/test")
-	public String testFlux(
-			@RequestParam(value = "nb") Integer number,
-			@RequestParam(value = "search") String texte
-			) {
-		return "Test : \n " +"guid = " + number +"\n"+ "titre = " + texte;
+	public GetController(ItemRepository itemRepository) {
+		
+		this.itemRepository = itemRepository;
 	}
+
+	@GetMapping //Mappe une méthode à une requête HTTP GET
+	public List<Item> getAllItems() {
+        return itemRepository.findAll();
+    }
+	
+//	@GetMapping("/guid")
+//	public String getRSSinXML(
+//			@RequestParam(value = "guid") String texte //Récupère un paramètre de l'URL
+//			) {
+//		return "Détail du contenu RSS demandé " + texte;
+//	}
+//	
+//	@GetMapping("/test")
+//	public String testFlux(
+//			@RequestParam(value = "nb") Integer number,
+//			@RequestParam(value = "search") String texte
+//			) {
+//		return "Test : \n " +"guid = " + number +"\n"+ "titre = " + texte;
+//	}
 }
--- src/main/java/fr/univrouen/rss25SB/model/Item.java
+++ src/main/java/fr/univrouen/rss25SB/model/Item.java
@@ -1,34 +1,94 @@
 package fr.univrouen.rss25SB.model;
 
+import jakarta.persistence.*;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlAttribute;
 import jakarta.xml.bind.annotation.XmlElement;
 import jakarta.xml.bind.annotation.XmlRootElement;
 
+@Entity
 @XmlRootElement(name = "item")
 @XmlAccessorType(XmlAccessType.PROPERTY)
-public class Item {
-    @XmlAttribute
-    private String guid;
-    @XmlElement
-    private String title;
-    @XmlElement
-    private String published;
+public class Item{
+	@Id
+	@XmlAttribute
+	private String guid;
+	
+	@XmlElement
+	private String title;
+	
+	@XmlElement
+	private String published;
+	
+	
+	public Item() {
+    }
+	
+	public Item(String guid, String title, String published) {
+      super();
+      this.guid = guid;
+      this.title = title;
+      this.published = published;
+  }
+	public String getGuid() {
+	   return guid;
+	  }
 
-    public Item(String guid, String title, String published) {
-        super();
-        this.guid = guid;
-        this.title = title;
+	 public void setGuid(String guid) {
+	     this.guid = guid;
+	    }
+
+	 public String getTitle() {
+	     return title;
+	    }
+
+	 public void setTitle(String title) {
+	     this.title = title;
+	    }
+
+	 public String getPublished() {
+		 return published;
+	    }
+
+	 public void setPublished(String published) {
         this.published = published;
-    }
-
-    public Item() {
-    }
-
-    @Override
-    public String toString() {
-        return ("Article : " + title + "\n(" + guid
-                + ") Le = " + published);
-    }
+	    }
+	
+	
 }
+
+
+
+//import jakarta.xml.bind.annotation.XmlAccessType;
+//import jakarta.xml.bind.annotation.XmlAccessorType;
+//import jakarta.xml.bind.annotation.XmlAttribute;
+//import jakarta.xml.bind.annotation.XmlElement;
+//import jakarta.xml.bind.annotation.XmlRootElement;
+//
+//@XmlRootElement(name = "item")
+//@XmlAccessorType(XmlAccessType.PROPERTY)
+//public class Item {
+//    @XmlAttribute
+//    private String guid;
+//    @XmlElement
+//    private String title;
+//    @XmlElement
+//    private String published;
+//
+//    public Item(String guid, String title, String published) {
+//        super();
+//        this.guid = guid;
+//        this.title = title;
+//        this.published = published;
+//    }
+//
+//    public Item() {
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return ("Article : " + title + "\n(" + guid
+//                + ") Le = " + published);
+//    }
+//}

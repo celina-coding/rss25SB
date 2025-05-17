a089f7525faa8ed2d9de7861bbe10c440bfb6800
celinaFeghoul <celinafeghoul@gmail.com>
2025-05-17 17:46:25 +0200

    another fix

--- src/main/java/fr/univrouen/rss25SB/Repository/AuteurContriuteur.java
+++
@@ -1,9 +0,0 @@
-package fr.univrouen.rss25SB.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.univrouen.rss25SB.model.Item;
-
-public interface AuteurContriuteur extends JpaRepository<Item, String>{
-
-}
--- src/main/java/fr/univrouen/rss25SB/Repository/LinkRepository.java
+++
@@ -1,9 +0,0 @@
-package fr.univrouen.rss25SB.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import fr.univrouen.rss25SB.model.Item;
-
-public interface LinkRepository extends JpaRepository<Item, String>{
-
-}
